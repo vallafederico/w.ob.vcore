eb660f74408b7817a26b0a3b76d77bd8651ea013
vallafederico <valla.federico@gmail.com>
2022-12-21 16:09:38 +0100

    cookie and video preloader

--- src/gl/util/loader.js
+++ src/gl/util/loader.js
@@ -14,26 +14,28 @@ export default class {
     gsap.set(this.page, { autoAlpha: 0 });
 
     this.video = document.querySelector('[data-loader="video"]');
-    this.video.addEventListener("ended", () => {});
-    this.video.play();
 
-    // console.log(this.video);
+    this.videoCookie = handleCookie();
+    // console.log("videocookie", this.videoCookie);
   }
 
   async load() {
     if (geoRedirect()) return; // geo redirect
-    console.time("load");
 
-    const [model, mt_black, mt_gold, tx_faces, mt_metal] = await Promise.all([
-      loadModel(assets.model),
-      loadTexture(assets.mt_black),
-      loadTexture(assets.mt_gold),
-      loadTexture(assets.tx_faces),
-      loadTexture(assets.mt_metal),
-    ]);
+    console.time("load");
+    const [model, mt_black, mt_gold, tx_faces, mt_metal, video] =
+      await Promise.all([
+        loadModel(assets.model),
+        loadTexture(assets.mt_black),
+        loadTexture(assets.mt_gold),
+        loadTexture(assets.tx_faces),
+        loadTexture(assets.mt_metal),
+        this.videoCookie
+          ? this.fadeVideoIn()
+          : new Promise((resolve) => resolve()),
+      ]);
 
     tx_faces.flipY = false;
-
     console.timeEnd("load");
 
     this.fadeOut();
@@ -47,6 +49,13 @@ export default class {
       tx_faces,
       mt_metal,
     };
+  }
+
+  async fadeVideoIn() {
+    return new Promise((resolve) => {
+      this.video.addEventListener("ended", () => resolve());
+      this.video.play();
+    });
   }
 
   fadeVideo() {
@@ -77,5 +86,46 @@ export default class {
         });
       },
     });
+  }
+}
+
+// cookies
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function handleCookie() {
+  // console.log(document.cookie);
+
+  const cname = "vcorevideo";
+  const cval = "true";
+
+  const vcookie = getCookie(cname);
+  // console.log("vcookie", vcookie);
+
+  if (vcookie) {
+    return false;
+  } else {
+    const exdays = 1;
+
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+
+    document.cookie = cname + "=" + cval + ";" + expires;
+
+    return true;
   }
 }
